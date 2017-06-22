160a13a32a41ad7aac17e97fb47a5a4bdb40044a
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-06-22 18:45:32 -0300

    ajuste no teste com a inclusão do classe que contem o método para teste

--- test/test-register-a.js
+++ test/test-register-a.js
@@ -1,8 +1,10 @@
-const expect  = require('chai').expect
+const expect  = require('chai').expect;
+
+const { registerA } = require('../src/registerA');
 
 describe('#registerA', () => {
-  it('return correct umbler.com ip', function() {
-    r = registerA('umbler.com')
+  it('return correct umbler.com ip', async function() {
+    r = await registerA('umbler.com')
     expect(r.address).to.equal('177.55.120.23')
   });
 });
