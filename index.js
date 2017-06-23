e052b0912f5d0c83eb9a09c5ca41d5b58b7cd83d
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-06-23 14:44:10 -0300

    mudança de api para lib

---
+++ index.js
@@ -0,0 +1,5 @@
+const { namesServers } = require('../src/namesServers');
+const { registerA } = require('../src/registerA');
+const { whoisParse } = require('../src/whoisParse');
+
+module.exports = { namesServers, registerA, whoisParse };
