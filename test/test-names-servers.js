a7a769cd50178739ec5ff4f2ecf99e9e17f31013
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-06-22 21:26:15 -0300

    inclusão de novos testes no métode de retorno de servidores de nome

--- test/test-names-servers.js
+++ test/test-names-servers.js
@@ -9,7 +9,21 @@ describe('#namesServers', () => {
     expect(r.length).to.equal(4);
     expect(r[0]).to.be.an('object');
     expect(r[0]).to.include.keys(['name','type','class','ttl', 'data']);
-    expect(r[0].data).to.deep.equal('ns1.umbler.com');
+    expect(nss).to.deep.include('ns1.umbler.com');
     expect(nss).to.deep.equal(['ns1.umbler.com','ns2.umbler.com','ns3.umbler.com','ns4.umbler.com']);
+  });
+
+  it('return names servers of google.com', async function() {
+    r = await namesServers('google.com');
+    nss = await r.map((ns) => { return ns.data });
+    expect(r.length).to.equal(4);
+    expect(r[0]).to.be.an('object');
+    expect(r[0]).to.include.keys(['name','type','class','ttl', 'data']);
+    expect(nss).to.deep.include('ns1.google.com');
+  });
+
+  it('return invalid domain', async function() {
+    r = await namesServers('enderecoquenaoexiste.com');
+    expect(r).to.deep.equal({})
   });
 });
